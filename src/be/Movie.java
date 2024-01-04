08d80abace87ee8c64310ea45e477919362d1d05
Ionut Andrei Grosu <80783004+grosuionutandrei@users.noreply.github.com>
2024-01-04 12:45:34 +0100

    business entities

--- src/be/Movie.java
+++ src/be/Movie.java
@@ -9,11 +9,70 @@ public class Movie {
     private Double rating;
     private String fileLink;
     private Date lastView;
+    private List<Category> categories;
 
+    public Movie(int id, String name, Double rating, String fileLink, Date lastView, List<Category> categories) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+        this.fileLink = fileLink;
+        this.lastView = lastView;
+        this.categories = categories;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public Double getRating() {
+        return rating;
+    }
 
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getFileLink() {
+        return fileLink;
+    }
+
+    public void setFileLink(String fileLink) {
+        this.fileLink = fileLink;
+    }
+
+    public Date getLastView() {
+        return lastView;
+    }
+
+    public void setLastView(Date lastView) {
+        this.lastView = lastView;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public Movie(String name, Double rating, String fileLink, Date lastView, List<Category> categories) {
+        this.name = name;
+        this.rating = rating;
+        this.fileLink = fileLink;
+        this.lastView = lastView;
+        this.categories = categories;
+    }
 }
