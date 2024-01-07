5f70e3ac8038bd1f49162bdc77c6a65831b836fc
Ionut Andrei Grosu <80783004+grosuionutandrei@users.noreply.github.com>
2024-01-07 08:32:34 +0100

    movies data displayed into the table plus categorie per movie

--- src/dal/MovieDao.java
+++ src/dal/MovieDao.java
@@ -11,6 +11,7 @@ import java.util.Date;
 
 public class MovieDao implements IMovieDao {
     private final ConnectionManager CONNECTION_MANAGER = new ConnectionManager();
+
     /**
      * retrieves the movie data from the database
      */
@@ -20,7 +21,7 @@ public class MovieDao implements IMovieDao {
         Movie movie;
         Category category;
         try (Connection conn = CONNECTION_MANAGER.getConnection()) {
-            String sql = "SELECT m.id,m.name, m.rating, m.filelink, m.lastview, m.personalRating,c.id,c.name FROM CatMovie cm " +
+            String sql = "SELECT m.id,m.name, m.rating, m.filelink, m.lastview, m.personalRating FROM CatMovie cm " +
                     "JOIN Movie m  on m.id = cm.MovieId " +
                     "JOIN Category c on c.id = cm.CategoryId " +
                     "WHERE c.id=?";
@@ -28,31 +29,20 @@ public class MovieDao implements IMovieDao {
             pstmt.setInt(1, categoryId);
             try (ResultSet rs = pstmt.executeQuery()) {
                 while (rs.next()) {
-                    System.out.println(rs.getDate(5));
                     int movieId = rs.getInt(1);
                     String movieName = rs.getString(2);
                     double rating = rs.getDouble(3);
                     String fileLink = rs.getString(4);
                     Date lastView = rs.getDate(5);
                     double personalRating = rs.getDouble(6);
-                    int catId = rs.getInt(7);
-                    String categoryName = rs.getString(8);
-
                     movie = new Movie(movieId, movieName, rating, fileLink, lastView, personalRating, null);
-                    System.out.println(movie);
-                    category = new Category(catId, categoryName);
-                    System.out.println(category);
+                    List<Category> categories = getMovieCategories(movie.getId());
+                    movie.setCategories(categories);
+                    moviesMap.put(movie.getId(), movie);
 
-                    if (moviesMap.containsKey(movie.getId())) {
-                        moviesMap.get(movie.getId()).getCategories().add(category);
-                    } else {
-                        movie.getCategories().add(category);
-                        moviesMap.put(movie.getId(), movie);
-                    }
                 }
             }
         } catch (SQLException | MoviesException e) {
-            System.out.println(e.getMessage());
             throw new MoviesException(ExceptionsMessages.READING_FROMDB_FAILED);
         }
         return moviesMap;
@@ -60,6 +50,30 @@ public class MovieDao implements IMovieDao {
 
     public Map<Integer, Movie> getMovies(int categoryId) throws MoviesException {
         return retrieveMovies(categoryId);
+    }
+
+
+    private List<Category> getMovieCategories(int movieId) {
+        String sql = "SELECT c.id,c.name FROM CatMovie cm JOIN Category c on c.id=cm.CategoryId JOIN Movie m on m.id=cm.MovieId WHERE m.id=?";
+        List<Category> movieCategories = new ArrayList<>();
+        Category category;
+        try (Connection connection = CONNECTION_MANAGER.getConnection()) {
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setInt(1, movieId);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    int catId = rs.getInt(1);
+                    String name = rs.getString(2);
+                    category = new Category(catId, name);
+                    movieCategories.add(category);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (MoviesException e) {
+            throw new RuntimeException(e);
+        }
+        return movieCategories;
     }
 
 
