4962e902d39b71e6c3985a9695d73b2e66826810
Radwan <145333687+larqsine@users.noreply.github.com>
2024-01-08 12:43:42 +0100

    Update FilterManager.java

--- src/gui/filterSongs/FilterManager.java
+++ src/gui/filterSongs/FilterManager.java
@@ -15,47 +15,47 @@ public class FilterManager {
 //    private final Object searchButton;
     private ISearchGraphic searchGraphic;
     private Label infoLabel;
-    private Button SearchButton;
+    private Button searchButton;
     private TextField searchValue;
 
     public FilterManager(MainModel model) {
         this.model = model;
         this.searchGraphic = searchGraphic;
         this.infoLabel = infoLabel;
-//        this. searchButton = searchButton;
+        this. searchButton = searchButton;
         this. searchValue = searchValue;
     }
 
 
-//    public void applyFilter(ActionEvent event) {
-//        String filter = searchValue.getText();
-//        if (searchButton.getGraphic().getId().equals(GraphicIdValues.SEARCH.getValue())) {
-//            if (!filter.isEmpty()) {
-//                applySearchFilter(filter);
-//            } else {
-//                showInfoMessage(InformationalMessages.FILTER_EMPTY.getValue());
-//            }
-//        }
-//        else {
-//            clearSearchFilter();
-//        }
-//    }
-//
-//    private void applySearchFilter(String filter) {
-//        searchGraphic = new UndoGraphic();
-//        model.applyFilter(filter);
-//        infoLabel.setVisible(false);
-//        searchButton.setGraphic(searchGraphic.getGraphic());
-//        searchValue.setText("");
-//        searchValue.setEditable(false);
-//    }
-//
-//    private void clearSearchFilter() {
-//        searchGraphic = new SearchGraphic();
-//        searchButton.setGraphic(searchGraphic.getGraphic());
-//        searchValue.setEditable(true);
-//        model.resetFilter();
-//    }
+    public void applyFilter(ActionEvent event) {
+        String filter = searchValue.getText();
+        if (searchButton.getGraphic().getId().equals(GraphicIdValues.SEARCH.getValue())) {
+            if (!filter.isEmpty()) {
+                applySearchFilter(filter);
+            } else {
+                showInfoMessage(InformationalMessages.FILTER_EMPTY.getValue());
+            }
+        }
+        else {
+            clearSearchFilter();
+        }
+    }
+
+    private void applySearchFilter(String filter) {
+        searchGraphic = new UndoGraphic();
+        model.applyFilter(filter);
+        infoLabel.setVisible(false);
+        searchButton.setGraphic(searchGraphic.getGraphic());
+        searchValue.setText("");
+        searchValue.setEditable(false);
+    }
+
+    private void clearSearchFilter() {
+        searchGraphic = new SearchGraphic();
+        searchButton.setGraphic(searchGraphic.getGraphic());
+        searchValue.setEditable(true);
+        model.resetFilter();
+  }
 
     private void showInfoMessage(String message) {
         infoLabel.setVisible(true);
