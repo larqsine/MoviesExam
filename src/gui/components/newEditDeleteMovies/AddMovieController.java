6fe4f9513d81548e1f60ded288ab8a426022358a
Ionut Andrei Grosu <80783004+grosuionutandrei@users.noreply.github.com>
2024-01-11 14:41:00 +0100

    GenreOptions class

--- src/gui/components/newEditDeleteMovies/AddMovieController.java
+++ src/gui/components/newEditDeleteMovies/AddMovieController.java
@@ -1,4 +1,14 @@
 package gui.components.newEditDeleteMovies;
 
+import javafx.event.ActionEvent;
+
 public class AddMovieController {
+    public void openFileChoser(ActionEvent event) {
+    }
+
+    public void addNewSong(ActionEvent event) {
+    }
+
+    public void cancelAddNewSong(ActionEvent event) {
+    }
 }
