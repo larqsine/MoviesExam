379b42cd45670ab143d4b6eab9daa3226c3b5d86
Ionut Andrei Grosu <80783004+grosuionutandrei@users.noreply.github.com>
2024-01-11 14:51:37 +0100

    MovieGenres enum implemented

--- src/gui/components/newEditDeleteMovies/AddMovieController.java
+++ src/gui/components/newEditDeleteMovies/AddMovieController.java
@@ -1,8 +1,18 @@
 package gui.components.newEditDeleteMovies;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AddMovieController {
+public class AddMovieController implements Initializable {
+   private MovieModel model;
+    @FXML
+    private HBox genresContainer;
+
+
     public void openFileChoser(ActionEvent event) {
     }
 
@@ -10,5 +20,10 @@ public class AddMovieController {
     }
 
     public void cancelAddNewSong(ActionEvent event) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
--- src/gui/components/newEditDeleteMovies/MovieModel.java
+++ src/gui/components/newEditDeleteMovies/MovieModel.java
@@ -1,4 +1,20 @@
 package gui.components.newEditDeleteMovies;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class MovieModel {
+    private ObservableList<String> genres ;
+
+
+    public MovieModel() {
+        this.genres = FXCollections.observableArrayList();
+    }
+
+    private void  initializeGenres(){
+
+    }
+
+
+
 }
