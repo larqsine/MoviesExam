b41c39ccfc31b875bcb73409b16b7f116debde7a
Ionut Andrei Grosu <80783004+grosuionutandrei@users.noreply.github.com>
2024-01-13 08:49:57 +0100

    genre be implemented

---
+++ src/be/Genre.java
@@ -0,0 +1,34 @@
+package be;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Genre {
+    private Integer id;
+    private final SimpleStringProperty name;
+
+    public Genre(int id, SimpleStringProperty name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Genre(SimpleStringProperty name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+}
